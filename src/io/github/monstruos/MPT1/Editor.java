e3d4f568546defe3acde84dac61e6bc301db96d3
Ivan Sanin <ivan.sanin1244@yandex.ru>
2019-02-18 20:40:47 +0700

    #lab1 наработки класса "Редактор"

---
+++ src/io/github/monstruos/MPT1/Editor.java
@@ -0,0 +1,35 @@
+package io.github.monstruos.MPT1;
+
+import javax.swing.*;
+
+public class Editor extends JFrame {
+    private JButton exitButton;
+    private JButton historyButton;
+    private JButton helpButton;
+    private JTextField input;
+    private JSlider inputSlider;
+    private JSpinner inputBaseSpinner;
+    private JTextField result;
+    private JSlider resultSlider;
+    private JSpinner resultBaseSpinner;
+    private JButton a0Button;
+    private JButton a1Button;
+    private JButton cButton;
+    private JButton dButton;
+    private JButton a9Button;
+    private JButton a8Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a2Button;
+    private JButton a6Button;
+    private JButton aButton;
+    private JButton eButton;
+    private JButton a7Button;
+    private JButton a3Button;
+    private JButton bButton;
+    private JButton fButton;
+    private JButton BSButton;
+    private JButton executeButton;
+    private JButton CEButton;
+    private JButton dotButton;
+}
