00490f9eb9de0f1135e1a492eb4eac83d9ee5eb5
Stanislav Mayorov <mayorov-s-a@yandex.ru>
2019-02-24 22:35:21 +0700

    Немного доработал конвертер
    Необходимо сделать тесты на корректность работы, но вроде все
    ошибки устранены

--- src/io/github/monstruos/MPT1/Converter.java
+++ src/io/github/monstruos/MPT1/Converter.java
@@ -3,16 +3,17 @@ package io.github.monstruos.MPT1;
 import static java.lang.Math.round;
 
 public class Converter {
-    public static String convertToString (double number, char base, int precision) {
+    public static String convertToString(double number, char base, int precision) {
         StringBuilder strNumber = new StringBuilder();
 
         long intPart = (long) number;
         double fracPart = number - intPart;
 
-        while (intPart != 0) {
+        while (intPart > 0) {
             char digit = (char) (intPart % base);
-            digit += digit < 10 ? '0' : -10 + 'A';
-            strNumber.append(digit);
+
+            strNumber.append(convertDigit(digit, base));
+
             intPart /= base;
         }
 
@@ -23,10 +24,14 @@ public class Converter {
 
             for (int exp = 0; exp < precision - 1; ++exp) {
                 char digit = (char) (fracPart *= base);
+
                 strNumber.append(convertDigit(digit, base));
+
                 fracPart -= digit;
             }
+
             char digit = (char) round(fracPart * base);
+
             if (digit == base) {
                 digit--;
             }
@@ -37,23 +42,32 @@ public class Converter {
         return strNumber.length() == 0 ? "0" : strNumber.toString();
     }
 
-    public static double convertToDouble (String number, char base) {
+    public static double convertToDouble(String number, char base) {
         int pos = number.indexOf('.');
 
-        long intPart = Long.valueOf(number.substring(0, pos));
-        double fracPart = 0;
+        double result;
 
         if (pos != -1) {
+            long intPart = Long.parseLong(number.substring(0, pos), base);
+            double fracPart;
+
             String fracSubstr = number.substring(pos + 1);
-            fracPart = Double.valueOf(fracSubstr);
+            fracPart = Long.parseLong(fracSubstr, base);
+
             while (fracPart >= 1) {
                 fracPart /= base;
             }
-            for (int idx = 0; fracSubstr.charAt(idx) == '0'; ++idx) {
+
+            for (int idx = 0; idx < fracSubstr.length() && fracSubstr.charAt(idx) == '0'; ++idx) {
                 fracPart /= base;
             }
+
+            result = intPart + fracPart;
+        } else {
+            result = Long.parseLong(number, base);
         }
-        return intPart + fracPart;
+
+        return result;
     }
 
     public static char convertDigit(int digit, int base) {
