019cae97b0eabcb6aa12ab441ae39a2b279f79c3
Stanislav Mayorov <mayorov-s-a@yandex.ru>
2019-02-24 22:14:25 +0700

    Реализовал основную логику интерфейса

--- src/io/github/monstruos/MPT1/Editor.java
+++ src/io/github/monstruos/MPT1/Editor.java
@@ -1,9 +1,14 @@
 package io.github.monstruos.MPT1;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 
 public class Editor extends JFrame {
-    private JButton exitButton;
+    private static char SEPARATOR = '.';
+    private static String ZERO = "0";
+
     private JButton historyButton;
     private JButton helpButton;
     private JTextField input;
@@ -32,4 +37,113 @@ public class Editor extends JFrame {
     private JButton executeButton;
     private JButton CEButton;
     private JButton dotButton;
+    private JPanel rootPanel;
+
+    public Editor() {
+        setMinimumSize(new Dimension(400, 500));
+        setContentPane(rootPanel);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        inputBaseSpinner.setValue(16);
+        resultBaseSpinner.setValue(16);
+
+        executeButton.addActionListener(e -> execute());
+        a0Button.addActionListener(e -> addDigit(0));
+        a1Button.addActionListener(e -> addDigit(1));
+        a2Button.addActionListener(e -> addDigit(2));
+        a3Button.addActionListener(e -> addDigit(3));
+        a4Button.addActionListener(e -> addDigit(4));
+        a5Button.addActionListener(e -> addDigit(5));
+        a6Button.addActionListener(e -> addDigit(6));
+        a7Button.addActionListener(e -> addDigit(7));
+        a8Button.addActionListener(e -> addDigit(8));
+        a9Button.addActionListener(e -> addDigit(9));
+        aButton.addActionListener(e -> addDigit(10));
+        bButton.addActionListener(e -> addDigit(11));
+        cButton.addActionListener(e -> addDigit(12));
+        dButton.addActionListener(e -> addDigit(13));
+        eButton.addActionListener(e -> addDigit(14));
+        fButton.addActionListener(e -> addDigit(15));
+        dotButton.addActionListener(e -> addSeparator());
+        BSButton.addActionListener(e -> deleteSymbolFromInput());
+        CEButton.addActionListener(e -> clear());
+        inputBaseSpinner.addChangeListener(e -> setInputBaseValue((Integer) inputBaseSpinner.getValue()));
+        resultBaseSpinner.addChangeListener(e -> setResultBaseValue((Integer) resultBaseSpinner.getValue()));
+        inputSlider.addChangeListener(e -> setInputBaseValue(inputSlider.getValue()));
+        resultSlider.addChangeListener(e -> setResultBaseValue(resultSlider.getValue()));
+    }
+
+    private void setInputBaseValue(Integer value) {
+        setBaseValue(value, inputBaseSpinner, inputSlider);
+    }
+
+    private void setResultBaseValue(Integer value) {
+        setBaseValue(value, resultBaseSpinner, resultSlider);
+    }
+
+    private static void setBaseValue(Integer value, JSpinner spinner, JSlider slider) {
+        if (value < 2) {
+            value = 2;
+        }
+        if (value > 16) {
+            value = 16;
+        }
+
+        spinner.setValue(value);
+        slider.setValue(value);
+    }
+
+    private void clear() {
+        input.setText(ZERO);
+    }
+
+    private void deleteSymbolFromInput() {
+        String value = input.getText();
+        value = value.substring(0, value.length() - 1);
+
+        if (value.isEmpty()) {
+            value = ZERO;
+        }
+
+        input.setText(value);
+    }
+
+    private void addDigit(int digit) {
+        String value = input.getText();
+        final int base = inputSlider.getValue();
+
+        if (digit < base) {
+            if (value.equals(ZERO)) {
+                value = "";
+            }
+            input.setText(value + Converter.convertDigit(digit, base));
+        }
+    }
+
+    private void addSeparator() {
+        final String value = input.getText();
+        if (value.indexOf(SEPARATOR) == -1) {
+            input.setText(value + SEPARATOR);
+        }
+    }
+
+    private void execute() {
+        final String inputValue = input.getText();
+        final int inputBase = inputSlider.getValue();
+
+        final int precision = inputValue.length() - inputValue.indexOf('.');
+
+        final double value = Converter.convertToDouble(inputValue, (char) inputBase);
+
+        final int resultBase = resultSlider.getValue();
+
+        final String resultValue = Converter.convertToString(value, (char) resultBase, precision);
+
+        result.setText(resultValue);
+    }
+
+    public static void main(String[] args) {
+        new Editor();
+    }
 }
