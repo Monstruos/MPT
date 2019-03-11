804f962d336d5ea4d47837eb96234b88adc4eb4b
Stanislav Mayorov <mayorov-s-a@yandex.ru>
2019-03-11 21:05:51 +0700

    Сделал форму для чисел в определенной базе
    Функционала - ноль, только компоновка

--- src/io/github/monstruos/MPT2/MainWindow.java
+++ src/io/github/monstruos/MPT2/MainWindow.java
@@ -1,4 +1,57 @@
 package io.github.monstruos.MPT2;
 
-public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    private JPanel rootPanel;
+    private JButton settingsButton;
+    private JButton helpButton;
+    private JTextField inputField;
+    private JButton BSButton;
+    private JButton CEButton;
+    private JButton clearButton;
+    private JButton MCButton;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton divButton;
+    private JButton sqrButton;
+    private JButton MRButton;
+    private JButton MSButton;
+    private JButton mButton;
+    private JButton aButton;
+    private JButton a4Button;
+    private JButton a1Button;
+    private JButton a0Button;
+    private JButton bButton;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton mulButton;
+    private JButton invButton;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton subButton;
+    private JButton signButton;
+    private JButton separatorButton;
+    private JButton addButton;
+    private JButton eqButton;
+    private JButton cButton;
+    private JButton dButton;
+    private JButton eButton;
+    private JButton fButton;
+    private JSlider baseSlider;
+    private JTextField baseField;
+
+    public MainWindow() {
+        setMinimumSize(new Dimension(500, 400));
+        setContentPane(rootPanel);
+        setVisible(true);
+        rootPanel.requestFocusInWindow();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
 }
